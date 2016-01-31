3012c1dcf640374bf7372881a2c56a98a53908a6
Geo3ngel <GeoEngel.z@gmail.com>
2016-01-31 14:50:09 -0800

    created the Event controller

---
+++ ScoutingAppServer/Controllers/EventController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using Microsoft.Azure.Mobile.Server.Config;
+
+namespace flipyserverService.Controllers
+{
+    [MobileAppController]
+    [RoutePrefix("api/events")]
+    public class EventController : ApiController
+    {
+        public HttpResponseMessage Refresh()
+        {
+            
+        }
+    }
+}
